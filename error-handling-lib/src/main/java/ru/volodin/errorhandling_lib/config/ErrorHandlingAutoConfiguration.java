d618b4268c6a01fe9410dc2eb31b4692fb416063
HzAjax <danil3000200@mail.ru>
2025-07-12 13:55:29 +0400

    Add baseHandler

--- error-handling-lib/src/main/java/ru/volodin/errorhandling_lib/config/ErrorHandlingAutoConfiguration.java
+++ error-handling-lib/src/main/java/ru/volodin/errorhandling_lib/config/ErrorHandlingAutoConfiguration.java
@@ -2,14 +2,20 @@ package ru.volodin.errorhandling_lib.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import ru.volodin.errorhandling_lib.exception.ControllerExceptionHandler;
+import ru.volodin.errorhandling_lib.exception.BaseExceptionHandler;
 
 @AutoConfiguration
+@EnableConfigurationProperties(ErrorHandlingProperties.class)
 public class ErrorHandlingAutoConfiguration {
 
     @Bean
-    public ControllerExceptionHandler controllerExceptionHandler(ObjectMapper objectMapper) {
-        return new ControllerExceptionHandler(objectMapper);
+    @ConditionalOnProperty(prefix = "app.errorhandling", name = "enabled", havingValue = "true", matchIfMissing = true)
+    @ConditionalOnMissingBean
+    public BaseExceptionHandler controllerExceptionHandler(ObjectMapper objectMapper) {
+        return new BaseExceptionHandler(objectMapper);
     }
 }
--- logging-lib/src/main/java/ru/volodin/logging_lib/config/LoggingAutoConfiguration.java
+++ logging-lib/src/main/java/ru/volodin/logging_lib/config/LoggingAutoConfiguration.java
@@ -4,10 +4,12 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import ru.volodin.logging_lib.filter.LoggingFilter;
 
 @AutoConfiguration
+@EnableConfigurationProperties(LoggingProperties.class)
 public class LoggingAutoConfiguration {
 
     @Bean
